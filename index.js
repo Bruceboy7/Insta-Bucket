d7f1339dd86f37157e5794f00d9b5fea883f7bf1
Bryson Bruce <brysonkbruce@gmail.com>
2021-03-24 17:45:12 -0500

    Added Local Storage

--- index.js
+++ index.js
@@ -1,79 +1,129 @@
-(function () {
- 
-})()
+let bucket = {
+  "name" : ["Stephanie", "Voltaire", "Bryson"],
+  "genre" : ["Jazz", "Blues", "R & B", "Pop", "Trap/Dubstep"],
+  "singer" : ["Alicia Keys", "Michael Buble", "John Legend", "Lawrence", "Sumi Jo", "Boyz II Men"],
+  "tempo" : ["rubato", 93, 127, 58, 74, 139],
+  "randomBeat" : [1, 2, 3, 4, 5, 6, 7, 8],
+  "goals" : ["Jacob Collier", "ReHarm", "Sound Scaping/FX Heavy", "Accapella/Choir", "Rap/Word Heavy", "Percussion Heavy", "Acoustic" ,"staccato", "legato"],
+  "keyword" : ["rain", "hunger", "floating", "rebound", "penelope", "Narnia", "time travel", "Civility", "pots & pans", "sapphire"]
+}
 
+let savedBucket = {}
+;(function () {
+
+  const saved = () => {
+    savedBucket = JSON.parse(window.localStorage.getItem("savedBucket"))
+    console.log(savedBucket)
+    
+    return("Saved Bucket is Loaded")
+  }
+  
+ console.log((window.localStorage.getItem("savedBucket")) ? saved() : "no saved bucket") 
+
+
+})()
+const saveBucket = () => {
+  console.log("saving bucket")
+  window.localStorage.setItem('savedBucket', JSON.stringify(bucket))
+  return((window.localStorage.getItem("savedBucket")) ? "bucket saved" : "no bucket saved for some reason")
+}
+const clearBucket = () => {
+  bucket = {
+    "name" : [],
+    "genre" : [],
+    "singer" : [],
+    "tempo" : [],
+    "randomBeat" : [],
+    "goals" : [],
+    "keyword" : [],
+    "randomBeat" : [1,2,3,4,5,6,7,8]
+  }
+  populateList()
+  console.log("List Cleared")
+}
+const loadBucket = () => {
+  bucket = savedBucket
+
+  $genreInput.value = ''
+  $singerInput.value = ''
+  $tempoInput.value = ''
+  $goalsInput.value = ''
+  $keywordInput.value = ''
+  $beatInput.value = ''
+
+  populateList()
+
+}
 const populateList = () => {
-  renderList(genre, $genreList)
-  renderList(singer, $singerList)
-  renderList(tempo, $tempoList)
-  renderList(goals, $goalsList)
-  renderList(keyword, $keywordList)
+  renderList(bucket["genre"], $genreList)
+  renderList(bucket["singer"], $singerList)
+  renderList(bucket["tempo"], $tempoList)
+  renderList(bucket["goals"], $goalsList)
+  renderList(bucket["keyword"], $keywordList)
 
 }
 const renderList = (categoryArray, categoryList) => {
 
   categoryList.innerHTML = ''
+  if (categoryArray.length == 0) categoryList.innerHTML += `<li>Empty. Add to your list!</li>`
   for (let i = 0; i < categoryArray.length; i++){
-    categoryList.innerHTML += `<li>${categoryArray[i]}</li>`
+    categoryList.innerHTML += `<li>${categoryArray[i] || "empty"}</li>`
 
   }
   
 }
-
-
 const randomize = () => {
   const randomIndex = (source) => {
-
+  if (source.length == 0) return "List empty. Submit -->"
   return (source[Math.floor(Math.random() * source.length)])
 
   }
 
-  $genreInput.value = randomIndex(genre)
-  $singerInput.value = randomIndex(singer)
-  $tempoInput.value = randomIndex(tempo)
-  $goalsInput.value = randomIndex(goals)
-  $keywordInput.value = randomIndex(keyword)
-  $beatInput.value = randomIndex(randomBeat)
+  $genreInput.value = randomIndex(bucket["genre"])
+  $singerInput.value = randomIndex(bucket["singer"])
+  $tempoInput.value = randomIndex(bucket["tempo"])
+  $goalsInput.value = randomIndex(bucket["goals"])
+  $keywordInput.value = randomIndex(bucket["keyword"])
+  $beatInput.value = randomIndex(bucket["randomBeat"])
   
   
   return("randomizing...")
   
 }
-let submitIt = (category) => {
+const submitIt = (category) => {
   
   switch (category) {
     case "genre":
       if ($genreInput.value !== ""){
-     (confirm(`do you want to add ${$genreInput.value} to your list?`)) ? genre[genre.length] = $genreInput.value : console.log("nvm")}
-     renderList(genre, $genreList)
+     (confirm(`do you want to add ${$genreInput.value} to your list?`)) ? bucket["genre"][bucket["genre"].length] = $genreInput.value : console.log("nvm")}
+     renderList(bucket["genre"], $genreList)
       break
     case "singer":
       if ($singerInput.value !== "") {
-      (confirm(`do you want to add ${$singerInput.value} to your list?`)) ? singer[singer.length] = $singerInput.value : console.log("nvm")}
-      renderList(singer, $singerList)
+      (confirm(`do you want to add ${$singerInput.value} to your list?`)) ? bucket["singer"][bucket["singer"].length] = $singerInput.value : console.log("nvm")}
+      renderList(bucket["singer"], $singerList)
       break
     case "tempo":
       if ($tempoInput.value !== "") {
-      (confirm(`do you want to add ${$tempoInput.value} to your list?`)) ? tempo[tempo.length] = $tempoInput.value : console.log("nvm")}
-      renderList(tempo, $tempoList)
+      (confirm(`do you want to add ${$tempoInput.value} to your list?`)) ? bucket["tempo"][bucket["tempo"].length] = $tempoInput.value : console.log("nvm")}
+      renderList(bucket["tempo"], $tempoList)
       break
     case "goals":
       if ($goalsInput.value !== "") {
-      (confirm(`do you want to add ${$goalsInput.value} to your list?`)) ? goals[genre.length] = $goalsInput.value : console.log("nvm")}
-      renderList(goals, $goalsList)
+      (confirm(`do you want to add ${$goalsInput.value} to your list?`)) ? bucket["goals"][bucket["goals"].length] = $goalsInput.value : console.log("nvm")}
+      renderList(bucket["goals"], $goalsList)
       break
     case "keyword":
       if ($keywordInput.value !== "") {
-      (confirm(`do you want to add ${$keywordInput.value} to your list?`)) ? keyword[keyword.length] = $keywordInput.value : console.log("nvm")}
-      renderList(keyword, $keywordList)
+      (confirm(`do you want to add ${$keywordInput.value} to your list?`)) ? bucket["keyword"][bucket["keyword"].length] = $keywordInput.value : console.log("nvm")}
+      renderList(bucket["keyword"], $keywordList)
       break
     default:
       break
 
   }
 }
-let showList = () => {
-  
+const showList = () => {
     if ($lists.style.visibility === 'hidden') {
       $lists.style.visibility = 'visible';
       populateList()
@@ -82,15 +132,6 @@ let showList = () => {
     }
   }
 
-  let name = ["Stephanie", "Voltaire", "Bryson"]
-  let genre = ["Jazz", "Blues", "R & B", "Pop", "Trap/Dubstep"]
-  let singer = ["Alicia Keys", "Michael Buble", "John Legend", "Lawrence", "Sumi Jo", "Boyz II Men"]
-  let tempo = ["rubato", 93, 127, 58, 74, 139]
-  let randomBeat = [1, 2, 3, 4, 5, 6, 7, 8]
-  let goals = ["Jacob Collier", "ReHarm", "Sound Scaping/FX Heavy", "Accapella/Choir", "Rap/Word Heavy", "Percussion Heavy", "Acoustic" ,"staccato", "legato"]
-  let keyword = ["rain", "hunger", "floating", "rebound", "penelope", "Narnia", "time travel", "Civility", "pots & pans", "sapphire"]
-
-  
   let $genreInput = document.getElementById("genreInput")
   let $singerInput = document.getElementById("singerInput")
   let $tempoInput = document.getElementById("tempoInput")
@@ -108,6 +149,7 @@ let showList = () => {
   let $beatList = document.getElementById("beatList")
 
   $randomize.addEventListener("click", randomize)
+  $lists.style.visibility = "hidden"
   $showList.addEventListener("click", showList)
   let submits = document.getElementsByClassName("inputBtn")
   for (let i = 0; i < submits.length; i++){
